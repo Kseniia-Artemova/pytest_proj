489bdb743fb260ea9a59be261244630767fe930f
Ksu <kseniyaartemova@yandex.ru>
2023-05-04 03:42:16 +0200

    added fixture in test_dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -2,21 +2,24 @@ import pytest
 from utils.dicts import get_val
 
 
-def test_get_val():
+@pytest.fixture
+def dict_():
     current_dict = {1: "one", 2: "two", 3: "three", 4: "four"}
-    assert get_val(current_dict, 2) == "two"
-    assert get_val(current_dict, 3) == "three"
+    return current_dict
 
 
-def test_get_val_not_key():
-    current_dict = {1: "one", 2: "two", 3: "three", 4: "four"}
-    assert get_val(current_dict, 9) == "git"
-    assert get_val(current_dict, 11, default="tig") == "tig"
+def test_get_val(dict_):
+    assert get_val(dict_, 2) == "two"
+    assert get_val(dict_, 3) == "three"
 
 
-def test_get_val_default_value():
-    current_dict = {1: "one", 2: "two", 3: "three", 4: "four"}
-    assert get_val(current_dict, 1, default="tig") == "one"
+def test_get_val_not_key(dict_):
+    assert get_val(dict_, 9) == "git"
+    assert get_val(dict_, 11, default="tig") == "tig"
+
+
+def test_get_val_default_value(dict_):
+    assert get_val(dict_, 1, default="tig") == "one"
 
 
 def test_get_val_empty_dict():
